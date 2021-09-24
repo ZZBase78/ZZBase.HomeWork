51d3e58188d31ca8e447c93c8eb3aae3c64efa4c
ZZBase78 <ZZBase2mail.ru>
2021-09-24 18:55:14 +0300

    Тестирование главного окна приложения

---
+++ Test_Main_Window/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Test_Main_Window
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newFormToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.ыавапрToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.showMessagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideMessageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newFormToolStripMenuItem,
+            this.showMessagesToolStripMenuItem,
+            this.hideMessageToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newFormToolStripMenuItem
+            // 
+            this.newFormToolStripMenuItem.Name = "newFormToolStripMenuItem";
+            this.newFormToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.newFormToolStripMenuItem.Text = "NewForm";
+            this.newFormToolStripMenuItem.Click += new System.EventHandler(this.newFormToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // ыавапрToolStripMenuItem
+            // 
+            this.ыавапрToolStripMenuItem.Name = "ыавапрToolStripMenuItem";
+            this.ыавапрToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ыавапрToolStripMenuItem.Text = "ыавапр";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 313);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(800, 115);
+            this.textBox1.TabIndex = 3;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Cursor = System.Windows.Forms.Cursors.HSplit;
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitter1.Location = new System.Drawing.Point(0, 310);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(800, 3);
+            this.splitter1.TabIndex = 4;
+            this.splitter1.TabStop = false;
+            // 
+            // showMessagesToolStripMenuItem
+            // 
+            this.showMessagesToolStripMenuItem.Name = "showMessagesToolStripMenuItem";
+            this.showMessagesToolStripMenuItem.Size = new System.Drawing.Size(102, 20);
+            this.showMessagesToolStripMenuItem.Text = "Show Messages";
+            this.showMessagesToolStripMenuItem.Click += new System.EventHandler(this.showMessagesToolStripMenuItem_Click);
+            // 
+            // hideMessageToolStripMenuItem
+            // 
+            this.hideMessageToolStripMenuItem.Name = "hideMessageToolStripMenuItem";
+            this.hideMessageToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.hideMessageToolStripMenuItem.Text = "Hide Message";
+            this.hideMessageToolStripMenuItem.Click += new System.EventHandler(this.hideMessageToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newFormToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ыавапрToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.ToolStripMenuItem showMessagesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideMessageToolStripMenuItem;
+    }
+}
+
---
+++ Test_Main_Window/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Main_Window
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newFormToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Form F = new Form2();
+            //F.MdiParent = this;
+            //F.Dock = DockStyle.Bottom;
+            ////F.BringToFront();
+            //F.Show();
+
+            Form F = new Form3();
+            F.MdiParent = this;
+            F.Show();
+        }
+
+        private void showMessagesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+            splitter1.Visible = true;
+        }
+
+        private void hideMessageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            splitter1.Visible = false;
+
+        }
+    }
+}
